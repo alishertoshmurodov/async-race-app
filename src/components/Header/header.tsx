125983e527094cb1bce220645a810afd61ee87b7
Alisher <toshmurodov2001@mail.ru>
2024-04-09 00:54:10 +0500

    fix: add color for the navigation buttons

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,7 +1,7 @@
 import "../../tailwind.css";
 import "./header.css";
 
-function Header({ setPage }: any) {
+function Header({ setPage, page }: any) {
   return (
     <header className="flex flex-col justify-center items-center max-w-6xl mx-auto gap-y-5">
       <h1 className="text-3xl font-bold md:text-6xl text-center uppercase">
@@ -9,10 +9,24 @@ function Header({ setPage }: any) {
       </h1>
 
       <nav className="flex gap-3 flex-col sm:flex-row ">
-        <button onClick={() => setPage("garage")} className="nav-button">
+        <button
+          onClick={() => setPage("garage")}
+          className={`nav-button ${
+            page === "garage"
+              ? "bg-gray-900 text-white border-gray-white cursor-default"
+              : null
+          }`}
+        >
           Garage
         </button>
-        <button onClick={() => setPage("winners")} className="nav-button">
+        <button
+          onClick={() => setPage("winners")}
+          className={`nav-button ${
+            page === "winners"
+              ? "bg-gray-900 text-white border-gray-white cursor-default"
+              : null
+          }`}
+        >
           Winners
         </button>
       </nav>
