b321e651a03565049f6df94aa3a4fe26a0f55dfd
Alisher <toshmurodov2001@mail.ru>
2024-05-06 19:33:11 +0500

    feat: add sorting to winners table

--- src/components/Winners/winners.tsx
+++ src/components/Winners/winners.tsx
@@ -1,8 +1,8 @@
-import './winners.css';
-import { useState } from 'react';
-import IconCarComponent from '../Garage/iconCarComponent';
-import { CarData } from '../../StateContext';
-import PaginationNav from '../Garage/paginationNav';
+import "./winners.css";
+import { useState } from "react";
+import IconCarComponent from "../Garage/iconCarComponent";
+import { CarData } from "../../StateContext";
+import PaginationNav from "../Garage/paginationNav";
 
 interface WinnersProps {
   winnersData: CarData[];
@@ -10,32 +10,72 @@ interface WinnersProps {
 
 function Winners({ winnersData }: WinnersProps) {
   const [currentPage, setCurrentPage] = useState(1);
+  const [sortCriteria, setSortCriteria] = useState<"wins" | "best">("wins");
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
 
   const itemsPerPage = 10;
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const totalPageCount = Math.ceil(winnersData.length / itemsPerPage);
 
+  const sortedData = [...winnersData].sort((a, b) => {
+    if (sortCriteria === "wins") {
+      return sortOrder === "asc" ? a.wins - b.wins : b.wins - a.wins;
+    } else {
+      return sortOrder === "asc" ? a.best - b.best : b.best - a.best;
+    }
+  });
+
   return (
     <section className="winners max-w-6xl mx-auto mt-10">
       {winnersData.length > 0 ? (
         <>
+          <div className="flex gap-3 items-center mb-2">
+            <div>
+              <span>Sort by:</span>
+              <select
+                value={sortCriteria}
+                onChange={(e) =>
+                  setSortCriteria(e.target.value as "wins" | "best")
+                }
+              >
+                <option value="wins">Wins</option>
+                <option value="best">Best Time</option>
+              </select>
+            </div>
+            <div className="flex gap-4">
+              <button
+                className={`border rounded-sm border-gray-700 p-1 ${sortOrder === "asc" ? "bg-gray-500 text-white" : ""}`}
+                onClick={() => setSortOrder("asc")}
+              >
+                asc
+              </button>
+              <button
+                className={`border rounded-sm border-gray-700 p-1 ${sortOrder === "desc" ? "bg-gray-500 text-white" : ""} `}
+                onClick={() => setSortOrder("desc")}
+              >
+                desc
+              </button>
+            </div>
+          </div>
           <table className="w-full border-collapse">
             <thead>
               <tr>
                 <th>№</th>
                 <th>Car</th>
                 <th>Name</th>
-                <th>Wins</th>
-                <th>Best Records (Seconds)</th>
+                <th onClick={() => setSortCriteria("wins")}>Wins</th>
+                <th onClick={() => setSortCriteria("best")}>
+                  Best Records (Seconds)
+                </th>
               </tr>
             </thead>
             <tbody>
-              {winnersData
+              {sortedData
                 .slice(indexOfFirstItem, indexOfLastItem)
-                .map((winner: CarData) => (
+                .map((winner: CarData, index: number) => (
                   <tr key={winner.id}>
-                    <td>{winner.id}</td>
+                    <td>{indexOfFirstItem + index + 1}</td>
                     <td>
                       <IconCarComponent color={winner.color} />
                     </td>
